6fc3a23a5a75db034dd4062f1fcf6a6736a7311d
Camilo Quezada <camiloquezadadev@gmail.com>
2020-07-21 15:01:44 -0400

    query post(id:) added

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,5 +6,24 @@ module Types
     def all_posts
       Post.all 
     end
+
+    field :post, PostType, null: true do  
+      argument :id, ID, required: true 
+    end
+    
+    def user(id:)
+      if context[:current_user] && context[:current_user].admin?
+        User.find_by(id: id, company_id: context[:current_company].id)
+      end
+      rescue ActiveRecord::RecordNotFound 
+        return nil
+    end
+
+    def post(id:)
+      Post.find(id)
+      rescue ActiveRecord::RecordNotFound
+        return nil
+    end
+    
   end
 end
