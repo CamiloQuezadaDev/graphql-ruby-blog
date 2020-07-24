b5888f10edc53be6cd3eb4518d0df88d534949d4
Camilo Quezada <camiloquezadadev@gmail.com>
2020-07-24 11:53:59 -0400

    validates email and password to user

--- app/graphql/mutations/sign_in.rb
+++ app/graphql/mutations/sign_in.rb
@@ -3,7 +3,8 @@ class Mutations::SignIn < Mutations::BaseMutation
     argument :credentials, Types::AuthProviderCredentialsInput, required: true
 
     field :user, Types::UserType, null: true 
-    field :token, String, null: true 
+    field :token, String, null: true
+    field :success, Boolean, null: false 
     field :errors, [String], null:true 
 
     def resolve(credentials: nil)
@@ -16,16 +17,19 @@ class Mutations::SignIn < Mutations::BaseMutation
 
         unless user.present? 
             result[:errors] = ["User doesn't found. Check email address"]
+            result[:success] = false 
             return result
         end
 
         unless credentials[:password].present?
             result[:errors] = ["Password required"]
+            result[:success] = false 
             return result 
         end
 
         unless user.authenticate(credentials[:password])
             result[:errors] = ["Wrong Password"]
+            result[:success] = false 
             return result 
         end
 
@@ -33,6 +37,6 @@ class Mutations::SignIn < Mutations::BaseMutation
 
         context[:session][:token] = token 
 
-        result = { user: user, token: token, errors: [] }
+        result = { user: user, token: token, errors: [], success: true }
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
     has_secure_password
-    
+
+    EMAIL_REGEXP = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
     validates :name, presence: true 
+
     validates :email, presence: true, uniqueness: true
+    validates_format_of :email, :with => EMAIL_REGEXP
+    
+    validates :password, length: {minimum: 7, maximum: 12 }
+
+
 end
