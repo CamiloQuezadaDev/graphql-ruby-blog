51becfa9844eb16fc6fcb739f57763bbe2c825e8
Camilo Quezada <camiloquezadadev@gmail.com>
2020-09-28 01:09:42 -0300

    add asset precompile just development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.assets.precompile += ['graphiql/rails/application.js', 'graphiql/rails/application.css']
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
